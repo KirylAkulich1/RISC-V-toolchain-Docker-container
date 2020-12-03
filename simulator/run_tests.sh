97fbab4b696522372c2201f7bf38fea771e96e74
Kiryl <kirillakulich@gmail.com>
2020-12-03 13:38:37 +0300

    add_tests

--- simulator/run_tests.sh
+++ simulator/run_tests.sh
@@ -4,5 +4,12 @@ echo $files
 for file in $files
 do
         echo "*** ${file} ***"
-        /mipt-mips/build/mipt-mips  -b /tests/benchmarks/$file  -I riscv64        
+        /mipt-mips/build/mipt-mips  -b /tests/benchmarks/$file  --isa riscv64 -d       
+done
+
+isa_files=$(ls /tests/benchmarks -a | grep  "[^\.dump]$")
+for file in $isa_files
+do
+        echo "*** ${file} ***"
+        /mipt-mips/build/mipt-mips  -b /tests/isa/$file  -isa riscv64 -n 800        
 done
